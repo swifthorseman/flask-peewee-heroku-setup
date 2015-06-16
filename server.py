f3f7b184ce95d5348059c3af6fabba92b13d7152
kinnik <northislandking@gmail.com>
2015-06-16 21:19:45 +0100

    Manage database connections explicitly - required for postgres - good practice

--- server.py
+++ server.py
@@ -1,7 +1,17 @@
-from flask import Flask, render_template
-from tellytubbies import retrieve_all
+from flask import Flask, render_template, g
+from tellytubbies import retrieve_all, db_proxy
 
 app = Flask(__name__)
+
+@app.before_request
+def before_request():
+    g.db = db_proxy
+    g.db.connect()
+
+@app.after_request
+def after_request(response):
+    g.db.close()
+    return response
 
 @app.route('/')
 def index():
